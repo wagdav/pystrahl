37c0fe8ecce462c0e94df3de3155eb1f9f9ca02b
David Wagner <david.wagner@epfl.ch>
2011-04-21 16:51:14 +0200

    New source shape: rectangle shaped with exponential tail.

--- strahl/__init__.py
+++ strahl/__init__.py
@@ -1,7 +1,7 @@
 __all__ = []
 
 from create_input import create_input
-from misc import rectangular_pulse
+from misc import rectangular_pulse, rectangular_pulse_with_decay
 from parameters import defaultParams
 
 import viz
--- strahl/misc.py
+++ strahl/misc.py
@@ -14,6 +14,36 @@ def rectangular_pulse(length, max_value, time_offset=0.0):
 
     return (t,y)
 
+
+def rectangular_pulse_with_decay(length, max_value, tau, t_offset=0.0):
+    r"""
+    Create a rectangular pulse with a *length* flattop followed by an
+    exponential decay that has a characteristic time *tau*
+
+      |-------|
+      |         -
+      |          \
+      |           -
+    __|            \_
+       <------><---->
+        length  tau
+    """
+    t, y = rectangular_pulse(length, max_value, t_offset)
+
+    t_decay = np.linspace(0, 5 * tau, 10)
+    y_decay = np.exp(-t_decay/tau)
+    t_decay += t_offset + length
+    y_decay *= max_value
+
+    y_decay[-1] = 0
+    t = np.hstack((t[:-1], t_decay))
+    y = np.hstack((y[:-1], y_decay))
+
+    return t, y
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod(verbose=False)
+
+    length = 1e-3
+    t, y = rectangular_pulse_with_decay(length, 1, tau=length)
