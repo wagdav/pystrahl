b3db5e858932b8922d19ea5c121fe1a17878abd3
David Wagner <david.wagner@epfl.ch>
2011-05-27 16:59:27 +0200

    Rewrite most of the analyis. Nicer, better, stronger.

--- gf_relation.py
+++ gf_relation.py
@@ -7,8 +7,6 @@ import strahl
 import gti
 import ppfit
 
-working_directory = './wk'
-
 
 def epsilon_prime(res):
     """
@@ -22,8 +20,21 @@ def epsilon_prime(res):
     sxr = res['sxr_radiation'][:,-1,:]
     impdens = res['total_impurity_density']
 
-    epsilonp = sxr / impdens
-    return time, rho_pol, epsilonp
+    rho_mask = rho_pol < 0.5
+
+    center = impdens[:, rho_mask]
+    maxpos = center.argmax()
+    maxpos = np.unravel_index(maxpos, center.shape)
+
+    #if maxpos[1] != 0:
+    #    print 'Cannot find the maximal impurity density in the center'
+    #    print maxpos[1]
+    #    raise AssertionError
+
+    time_eq = maxpos[0]
+
+    epsilonp = sxr[time_eq] / impdens[time_eq]
+    return rho_pol, epsilonp
 
 
 def plot_epsilon_prime():
@@ -44,160 +55,159 @@ def Gamma(dndt, r):
 
 
 def epsilon_on_new_radius_grid(epsilon, inverted_rho):
-    e_time, e_rho, e_data = epsilon
-
-    epsilon_new = np.zeros((len(e_time), len(inverted_rho)))
-    for i in xrange(len(e_time)):
-        epsilon_new[i] = np.interp(inverted_rho, e_rho, e_data[i])
-
-    return e_time, inverted_rho, epsilon_new
-
-
-def epsilon_on_new_time_grid(epsilon, inverted_time):
-    e_time, e_rho, e_data = epsilon
-
-    epsilon_new = np.zeros((len(inverted_time), len(e_rho)))
-    for i in xrange(len(e_rho)):
-        epsilon_new[:,i] = np.interp(inverted_time, e_time, e_data[:,i])
-
-    return inverted_time, e_rho, epsilon_new
-
-
-def plot_gf(rho_index=0):
-    ax = plt.gca()
-    x, y = select_influx_phase(rho_index)
-    ax.plot(x, y, '.', label=str(inverted_rho[rho_index]))
-
-
-def do_fit(rho_index):
-    x, y = select_influx_phase(rho_index)
-    D, v = np.polyfit(x, y, 1)
-    D = -D
-    return D, v
-
-
-def select_influx_phase(rho_index):
-    xsel = x[time_mask][:, rho_index]
-    ysel = y[time_mask][:, rho_index]
-
-    mask = np.gradient(xsel) < 0
-    xsel = xsel[mask]
-    ysel = ysel[mask]
-    return xsel, ysel
-
-
-def remap_epsilon(res, inversion):
-    inverted_rho, inverted_time, inverted = inversion
-
-    epsilon = epsilon_prime(res)
-    epsilon = epsilon_on_new_radius_grid(epsilon, inverted_rho)
-    epsilon = epsilon_on_new_time_grid(epsilon, inverted_time)
-
-    return epsilon
+    e_rho, e_data = epsilon
+    epsilon_new = np.interp(inverted_rho, e_rho, e_data)
+    return inverted_rho, epsilon_new
 
 
 def calculate_impurity_density(epsilon, inversion):
-    inverted_rho, inverted_time, inverted = inversion
-
-    offset = inverted[0:100,:]
+    offset = inversion.emissivity[0:100,:]
     offset = offset.mean(0)
     offset = offset[np.newaxis]
-    return (inverted - offset) / epsilon[2][991]
+
+    impurity_density = (inversion.emissivity - offset) / epsilon[1]
+    return impurity_density
 
 
-def smooth_impurity_density(time, impurity_density):
+def smooth_impurity_density(impurity_density):
+    time = impurity_density.time
+    rho = impurity_density.rho
+
+    dt_sample = time[1] - time[0]
+    dt_smooth = 0.02
+
+    k_dt = dt_smooth // dt_sample
+    pieces = len(time) // k_dt
+    print pieces
+
     dndt = []
-    impurity_density = []
-    for rho_index in xrange(40):
-        signal = impurity_density_exp[:,rho_index]
+    smooth = []
+    for rho_index, r in enumerate(rho):
+        signal = impurity_density.emissivity[:,rho_index]
 
-        p = ppfit.ppolyfit(time, signal, 20, deg=3)
+        p = ppfit.ppolyfit(time, signal, pieces, deg=3)
         dp = ppfit.ppolyder(p)
 
         y = ppfit.ppolyval(p, time)
         dy = ppfit.ppolyval(dp, time)
 
         dndt.append(dy)
-        impurity_density.append(y)
+        smooth.append(y)
 
     dndt = np.array(dndt)
-    impurity_density = np.array(impurity_density).T
+    smooth = np.array(smooth).T
+    dndt = gti.InversionData(impurity_density.shot, rho, time, dndt)
+    smooth = gti.InversionData(impurity_density.shot, rho, time, smooth)
 
-    return impurity_density, dndt
-
-
-def plot_impurity_density(rho_index=0):
-    ax = plt.gca()
-    ax.plot(time, impurity_density_exp[:, rho_index])
-    ax.plot(time, impurity_density[:, rho_index])
-    ax.plot(time[time_mask], impurity_density[time_mask][:, rho_index])
-
-    ax.text(0.95, 0.95, r'$\rho=%1.2f$' % inverted_rho[rho_index],
-            transform=ax.transAxes, ha='right', va='top')
-    ax.figure.canvas.draw()
+    return smooth, dndt
 
 
-def plot_impurity_gradient(rho_index=0):
-    ax = plt.gca()
-    ax.plot(time, x[:, rho_index])
-    ax.plot(time[time_mask], x[time_mask][:, rho_index])
+class GradientFlux(object):
+    def __init__(self, impurity_density_exp):
+        self.impurity_density_exp = impurity_density_exp
+        self.eval_gradient_flux()
 
-    ax.text(0.95, 0.95, r'$\rho=%1.2f$' % inverted_rho[rho_index],
-            transform=ax.transAxes, ha='right', va='top')
-    ax.figure.canvas.draw()
+    def _smooth(self):
+        smooth, dndt = smooth_impurity_density(self.impurity_density_exp)
+        return smooth, dndt
+
+    def eval_gradient_flux(self):
+        impurity_density, dndt = self._smooth()
+        rho = self.impurity_density_exp.rho
+        time = self.impurity_density_exp.time
+        rho_vol = np.interp(rho, res['rho_poloidal_grid'], res['radius_grid'])
+        G = Gamma(dndt.emissivity.T, rho_vol)
+
+        dndr = np.zeros_like(impurity_density.emissivity)
+        for i, t in enumerate(time):
+            dndr[i,:] = np.gradient(impurity_density.emissivity[i])/np.gradient(rho_vol)
+
+        x = dndr[:,:-1] / impurity_density.emissivity[:,:-1]
+        y = G / impurity_density.emissivity[:, :-1]
+
+        self.gradient = x
+        self.flux = y
+        return x, y
+
+    def plot_gradient(self, rho_index=0):
+        ax = plt.gca()
+        time = self.impurity_density_exp.time
+        rho = self.impurity_density_exp.rho
+
+        ax.plot(time, self.gradient[:, rho_index])
+        ax.text(0.95, 0.95, r'$\rho=%1.2f$' % rho[rho_index],
+                transform=ax.transAxes, ha='right', va='top')
+        ax.figure.canvas.draw()
+
+    def plot_flux(self, rho_index=0):
+        ax = plt.gca()
+        time = self.impurity_density_exp.time
+        rho = self.impurity_density_exp.rho
+        ax.plot(time, self.flux[:, rho_index])
+
+        ax.text(0.95, 0.95, r'$\rho=%1.2f$' % rho[rho_index],
+                transform=ax.transAxes, ha='right', va='top')
+        ax.figure.canvas.draw()
+
+    def plot_time_evolution(self):
+        ax = plt.gca()
+
+        d = self.impurity_density_exp
+        ax.plot(d.time, d.emissivity[:, ::5], '.')
+        smooth, dndt = self._smooth()
+        ax.plot(smooth.time, smooth.emissivity[:, ::5], lw=2)
+
+    def plot_gf(self, rho_index=0):
+        ax = plt.gca()
+        ax.plot(self.gradient[:,rho_index], self.flux[:, rho_index], '.',
+                label=str(inversion.rho[rho_index]))
+
+        D, v = self._fit_Dv(rho_index)
+        x =  self.gradient[:,rho_index]
+        ax.plot(x, -D * x+ v)
+
+    def _fit_Dv(self, rho_index):
+        D, v = np.polyfit(self.gradient[:,rho_index], self.flux[:,rho_index], 1)
+        D = -D
+        return D, v
+
+    def Dv_profile(self, left=0, right=0.6):
+        D = []
+        v = []
+        r = []
+
+        for i, rho in enumerate(self.impurity_density_exp.rho):
+            if rho < left or rho > right: continue
+            try:
+                D_, v_ = self._fit_Dv(i)
+            except TypeError:
+                continue
+
+            r.append(inversion.rho[i])
+            D.append(D_)
+            v.append(v_)
+
+        r = np.array(r)
+        D = np.array(D) / 1e4
+        v = np.array(v) / 1e2
+        return r, D, v
 
 
-def plot_impurity_flux(rho_index=0):
-    ax = plt.gca()
-    ax.plot(time, y[:, rho_index])
-    ax.plot(time[time_mask], y[time_mask][:, rho_index])
-
-    ax.text(0.95, 0.95, r'$\rho=%1.2f$' % inverted_rho[rho_index],
-            transform=ax.transAxes, ha='right', va='top')
-    ax.figure.canvas.draw()
-
-
+working_directory = './wk'
 of = os.path.join(working_directory, 'result', 'Arstrahl_result.dat')
 res = strahl.viz.read_results(of)
 
-inversion = gti.inverted_data(42661) # rho, time, data
-epsilon = remap_epsilon(res, inversion)
+inversion = gti.inverted_data(42661).select_time(0.5, 1.0)
 
-impurity_density_exp = calculate_impurity_density(epsilon, inversion)
-impurity_density, dndt = smooth_impurity_density(inversion[1], impurity_density_exp)
+epsilon = epsilon_prime(res)
+epsilon = epsilon_on_new_radius_grid(epsilon, inversion.rho)
 
-rho_vol = np.interp(inversion[0], res['rho_poloidal_grid'], res['radius_grid'])
-G = Gamma(dndt.T, rho_vol)
+impurity_density_exp = gti.InversionData(inversion.shot, inversion.rho,
+        inversion.time, inversion.emissivity / epsilon[1])
 
-# gradient
-dndr = np.zeros_like(impurity_density)
-for i in xrange(len(inversion[1])):
-    dndr[i,:] = np.gradient(impurity_density[i])/np.gradient(rho_vol)
-    l = dndr[i,:]
+gf = GradientFlux(impurity_density_exp.select_time(0.52, 0.56))
 
-x = dndr[:,:-1] / impurity_density[:,:-1]
-y = G / impurity_density[:, :-1]
-
-time_range = (0.5 + 0.02, 0.5 + 0.04)
-time_mask = (inversion[1] > time_range[0]) & (inversion[1] < time_range[1])
-
-D = []
-v = []
-r = []
-for i in xrange(3, 24):
-    try:
-        D_, v_ = do_fit(i)
-    except TypeError:
-        continue
-
-    if D_ > 0 and D_ < 200 and abs(v_) < 2000:
-        r.append(inversion[0][i])
-        D.append(D_)
-        v.append(v_)
-
-r = np.array(r)
-D = np.array(D)
-v = np.array(v)
+r,D,v = gf.Dv_profile(left=0.0, right=0.6)
 
 plt.figure(10); plt.clf()
 plt.subplot(211)
@@ -212,4 +222,3 @@ plt.show()
 np.savetxt('D_profile.txt', D)
 np.savetxt('r_profile.txt', r)
 np.savetxt('v_profile.txt', v)
-
